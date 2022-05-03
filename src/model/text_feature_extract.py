62adbbda23ed8ad5b32cfc5187d7db2227ee99f7
hou080146 <hou080146@163.com>
2022-05-03 23:33:52 +0800

    Changes

--- src/model/text_feature_extract.py
+++ src/model/text_feature_extract.py
@@ -3,7 +3,6 @@
 @author: zifyloo
 """
 
-
 from torch import nn
 import torch
 
@@ -13,13 +12,13 @@ class TextExtract(nn.Module):
     def __init__(self, opt):
         super(TextExtract, self).__init__()
 
-        self.embedding_local = nn.Embedding(opt.vocab_size, 512, padding_idx=0)
+        self.embedding_local = nn.Embedding(opt.vocab_size, 512, padding_idx=0)  # 生成大小为vocab_size（可以装这么多个词），维度为512的向量词典
         self.embedding_global = nn.Embedding(opt.vocab_size, 512, padding_idx=0)
-        self.dropout = nn.Dropout(0.3)
-        self.lstm = nn.LSTM(512, 2048, num_layers=1, bidirectional=True, bias=False)
+        self.dropout = nn.Dropout(0.3)  # 以0.3的概率扔掉神经元防止过拟合
+        self.lstm = nn.LSTM(512, 2048, num_layers=1, bidirectional=True, bias=False)  # 构建LSTM模型的网络
 
     def forward(self, caption_id, text_length):
-
+        # 构建前向传播模型，每一步表示处理步骤
         text_embedding_global = self.embedding_global(caption_id)
         text_embedding_global = self.dropout(text_embedding_global)
         text_embedding_global = self.calculate_different_length_lstm(text_embedding_global, text_length, self.lstm)
@@ -31,8 +30,8 @@ class TextExtract(nn.Module):
         return text_embedding_global, text_embedding_local
 
     def calculate_different_length_lstm(self, text_embedding, text_length, lstm):
-        text_length = text_length.view(-1)
-        _, sort_index = torch.sort(text_length, dim=0, descending=True)
+        text_length = text_length.view(-1)  # 改变维度
+        _, sort_index = torch.sort(text_length, dim=0, descending=True)  # dim=0按列排序，降序，返回两个值分别为排序后的数据和其在原矩阵中的下标
         _, unsort_index = sort_index.sort()
 
         sortlength_text_embedding = text_embedding[sort_index, :]
@@ -42,7 +41,6 @@ class TextExtract(nn.Module):
                                                                   sort_text_length,
                                                                   batch_first=True)
 
-        
         # self.lstm.flatten_parameters()
         packed_feature, _ = lstm(packed_text_embedding)  # [hn, cn]
         total_length = text_embedding.size(1)
